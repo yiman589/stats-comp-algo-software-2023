c5dc54d8c3c5b3a8bd183f9d90c0dc783b49d3ab
Yi Man <yman@compute-140.cm.cluster>
2023-02-12 16:43:53 -0500

    fixed some codes

--- homework/git_experiment/edit_and_commit.sh
+++ homework/git_experiment/edit_and_commit.sh
@@ -4,4 +4,7 @@ for commit_count in {2..100}
 do
   let linenum="10+$commit_count"
   sed -i "$linenum i This is sentence no.$commit_count." foo.tex
+  pdflatex foo.tex
+  git add -u
+  git commit -m "Add another sentence to TeX file"
 done
